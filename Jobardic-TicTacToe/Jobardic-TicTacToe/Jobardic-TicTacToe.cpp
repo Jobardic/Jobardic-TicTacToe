8bf61be934d3ecc277746c6e2cabd12674815dc8
MatthewM259 <mattmullen173@gmail.com>
2026-09-11 18:36:08 -0400

    JOBARDIC-5 Experimenting with utilizing code from multiple seperate files

--- Jobardic-TicTacToe/Jobardic-TicTacToe/Jobardic-TicTacToe.cpp
+++ Jobardic-TicTacToe/Jobardic-TicTacToe/Jobardic-TicTacToe.cpp
@@ -1,12 +1,29 @@
 // Jobardic-TicTacToe.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
+
+
 #include <iostream>
+#include "Jobardic-test_header.h"
+using namespace std;
 
 int main()
 {
-    std::cout << "TicTacToe project file created!\n";
+    cout << "TicTacToe project file created!\n";
+   testfunc();
 }
+
+
+
+
+
+
+
+
+
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
---
+++ Jobardic-TicTacToe/Jobardic-TicTacToe/Jobardic-test_file.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+void testfunc() {
+	cout << "test output from function in 'Jobardic-test_file.cpp'" << endl;
+}

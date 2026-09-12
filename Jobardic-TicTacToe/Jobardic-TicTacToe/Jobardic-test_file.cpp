67668eb03f5584fe06dc02136b0fe9d15ce3ef67
MatthewM259 <mattmullen173@gmail.com>
2026-09-12 12:40:40 -0400

    JOBARDIC-5 Experimenting with special character usage To use certain special characters instead of just the letters "X" and "O" for the tic tac toe board

--- Jobardic-TicTacToe/Jobardic-TicTacToe/Jobardic-test_file.cpp
+++ Jobardic-TicTacToe/Jobardic-TicTacToe/Jobardic-test_file.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 void testfunc() {
+
+	SetConsoleOutputCP(CP_UTF8); //sets the console output encoding version to be able to use those special symbols.
 	cout << "test output from function in 'Jobardic-test_file.cpp'" << endl;
+	cout << "Using Letters: X O " << endl;
+	cout << "Using special symbols which require setting encoding version to UTF8: ✕ 〇" << endl;
 }
